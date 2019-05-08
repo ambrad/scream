1efa82dc2ee0d8e2991d1d8878f8dfe43dbeb4f8
Andrew M. Bradley <ambradl@sandia.gov>
2019-05-07 20:43:48 -0600

    start perf driver

--- components/scream/src/share/tests/tridiag_tests.cpp
+++ components/scream/src/share/tests/tridiag_tests.cpp
@@ -7,6 +7,9 @@
 #include "share/scream_pack.hpp"
 #include "share/scream_pack_kokkos.hpp"
 #include "share/util/scream_arch.hpp"
+#include "share/util/scream_utils.hpp"
+
+#include <chrono>
 
 template <typename TridiagDiag>
 KOKKOS_INLINE_FUNCTION
@@ -110,6 +113,7 @@ struct Solver {
               thomas_scalar, thomas_pack,
               cr_scalar,
               error };
+
   static std::string convert (Enum e) {
     switch (e) {
     case thomas_team_scalar: return "thomas_team_scalar";
@@ -120,6 +124,7 @@ struct Solver {
     default: scream_require_msg(false, "Not a valid solver: " << e);
     }
   }
+
   static Enum convert (const std::string& s) {
     if (s == "thomas_team_scalar") return thomas_team_scalar;
     if (s == "thomas_team_pack") return thomas_team_pack;
@@ -138,7 +143,6 @@ Solver::Enum Solver::all[] = { thomas_team_scalar, thomas_team_pack,
 
 namespace test_correct {
 struct TestConfig {
-  using BulkLayout = Kokkos::LayoutRight;
   using TeamLayout = Kokkos::LayoutRight;
 
   Solver::Enum solver;
@@ -514,12 +518,284 @@ TEST_CASE("tridiag", "correctness") {
   }
 }
 
+namespace perf {
+void expect_another_arg (int i, int argc) {
+  if (i == argc-1)
+    throw std::runtime_error("Expected another cmd-line arg.");
+}
+
+struct Input {
+  struct Solver {
+    enum Enum { thomas, cr, error };
+
+    static std::string convert (Enum e) {
+      switch (e) {
+      case thomas: return "thomas";
+      case cr: return "cr";
+      default: scream_require_msg(false, "Not a valid solver: " << e);
+      }
+    }
+
+    static Enum convert (const std::string& s) {
+      if (s == "thomas") return thomas;
+      if (s == "cr") return cr;
+      return error;
+    }
+  };
+
+  Solver::Enum method;
+  int nprob, nrow, nrhs, nwarp;
+  bool pack, oneA;
+
+  Input ()
+    : method(Solver::cr), nprob(2048), nrow(128), nrhs(43), nwarp(-1),
+      pack( ! scream::util::OnGpu<Kokkos::DefaultExecutionSpace>::value),
+      oneA(true)
+  {}
+
+  bool parse (int argc, char** argv) {
+    using scream::util::eq;
+    for (int i = 1; i < argc; ++i) {
+      if (eq(argv[i], "-m", "--method")) {
+        expect_another_arg(i, argc);
+        method = Input::Solver::convert(argv[++i]);
+        if (method == Input::Solver::error) {
+          std::cout << "Not a solver: " << argv[i] << "\n";
+          return false;
+        }
+      } else if (eq(argv[i], "-np", "--nprob")) {
+        expect_another_arg(i, argc);
+        nprob = std::atoi(argv[++i]);
+      } else if (eq(argv[i], "-nr", "--nrow")) {
+        expect_another_arg(i, argc);
+        nrow = std::atoi(argv[++i]);
+      } else if (eq(argv[i], "-nc", "--nrhs")) {
+        expect_another_arg(i, argc);
+        nrhs = std::atoi(argv[++i]);
+      } else if (eq(argv[i], "-1a", "--oneA")) {
+        oneA = true;
+      } else if (eq(argv[i], "-nw", "--nwarp")) {
+        expect_another_arg(i, argc);
+        nwarp = std::atoi(argv[++i]);
+      } else if (eq(argv[i], "-nop", "--nopack")) {
+        pack = false;
+      } else {
+        std::cout << "Unexpected arg: " << argv[i] << "\n";
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+std::string string (const Input& in, const int& nwarp) {
+  std::stringstream ss;
+  ss << "run: solver " << Input::Solver::convert(in.method)
+     << " pack " << in.pack
+     << " nprob " << in.nprob
+     << " nrow " << in.nrow
+     << " nA " << (in.oneA ? 1 : in.nrhs)
+     << " nrhs " << in.nrhs
+     << " nwarp " << nwarp << "\n";
+  return ss.str();
+}
+
+template <typename ST, typename DT>
+void pack_data (const ST& a, DT b) {
+  using Pack = typename DT::non_const_value_type;
+  static_assert(Pack::packtag, "DT value type must be Pack");
+  static_assert(std::is_same<typename ST::non_const_value_type,
+                             typename Pack::scalar>::value,
+                "ST value and DT::Pack::scalar types must be the same");
+  const int np = a.extent_int(0);
+  const int m = a.extent_int(1), n = a.extent_int(2), mn = m*n;
+  assert(b.extent_int(0) == np);
+  assert(b.extent_int(1) == m);
+  assert(b.extent_int(2) == (n + Pack::n - 1)/Pack::n);
+  auto f = KOKKOS_LAMBDA (const int i) {
+    const int prob = i / mn;
+    const int row = (i % mn) / n;
+    const int col = i % n;
+    b(prob, row, col / Pack::n)[col % Pack::n] = a(prob,row,col);
+  };
+  Kokkos::parallel_for(np*m*n, f);
+}
+
+template <typename ST, typename DT>
+void unpack_data (const ST& a, DT b) {
+  using Pack = typename ST::non_const_value_type;
+  static_assert(Pack::packtag, "ST value type must be Pack");
+  static_assert(std::is_same<typename DT::non_const_value_type,
+                             typename Pack::scalar>::value,
+                "DT value and ST::Pack::scalar types must be the same");
+  const int np = b.extent_int(0);
+  const int m = b.extent_int(1), n = b.extent_int(2), mn = m*n;
+  assert(a.extent_int(0) == np);
+  assert(a.extent_int(1) == m);
+  assert(a.extent_int(2) == (n + Pack::n - 1)/Pack::n);
+  auto f = KOKKOS_LAMBDA (const int i) {
+    const int prob = i / mn;
+    const int row = (i % mn) / n;
+    const int col = i % n;
+    b(prob,row,col) = a(prob, row, col / Pack::n)[col % Pack::n];
+  };
+  Kokkos::parallel_for(np*m*n, f);
+}
+
+template <typename ST, typename DT>
+void pack_matrix (const ST& a, DT b, const int nrhs) {
+  using Pack = typename DT::non_const_value_type;
+  static_assert(Pack::packtag, "DT value type must be Pack");
+  static_assert(std::is_same<typename ST::non_const_value_type,
+                             typename Pack::scalar>::value,
+                "ST value and DT::Pack::scalar types must be the same");
+  const int np = a.extent_int(0);
+  const int nrow = a.extent_int(2);
+  assert(b.extent_int(0) == np);
+  assert(b.extent_int(1) == 3);
+  assert(b.extent_int(2) == nrow);
+  assert(b.extent_int(3) == (nrhs + Pack::n - 1)/Pack::n);
+  auto f = KOKKOS_LAMBDA (const int i) {
+    const int prob = i / (3*nrow*nrhs);
+    const int c = (i % (3*nrow*nrhs)) / (nrow*nrhs);
+    const int row = (i % (nrow*nrhs)) / nrhs;
+    const int rhs = i % nrhs;
+    b(prob, c, row, rhs / Pack::n)[rhs % Pack::n] = a(prob,c,row);
+  };
+  Kokkos::parallel_for(np*3*nrow*nrhs, f);
+}
+
+template <typename ST, typename DT>
+void pack_scalar_matrix (const ST& a, DT b, const int nrhs) {
+  const int np = a.extent_int(0);
+  const int nrow = a.extent_int(2);
+  assert(b.extent_int(0) == np);
+  assert(b.extent_int(1) == 3);
+  assert(b.extent_int(2) == nrow);
+  assert(b.extent_int(3) == nrhs);
+  auto f = KOKKOS_LAMBDA (const int i) {
+    const int prob = i / (3*nrow*nrhs);
+    const int c = (i % (3*nrow*nrhs)) / (nrow*nrhs);
+    const int row = (i % (nrow*nrhs)) / nrhs;
+    const int rhs = i % nrhs;
+    b(prob,c,row,rhs) = a(prob,c,row);
+  };
+  Kokkos::parallel_for(np*3*nrow*nrhs, f);
+}
+
+using BulkLayout = Kokkos::LayoutRight;
+using TeamLayout = Kokkos::LayoutRight;
+
+template <typename Scalar>
+using TridiagArrays = Kokkos::View<Scalar****, BulkLayout>;
+template <typename Scalar>
+using DataArrays = Kokkos::View<Scalar***, BulkLayout>;
+
+template <typename Real>
+void run (const Input& in) {
+  using Kokkos::create_mirror_view;
+  using Kokkos::deep_copy;
+  using Kokkos::subview;
+  using Kokkos::ALL;
+  using TeamPolicy = Kokkos::TeamPolicy<Kokkos::DefaultExecutionSpace>;
+  using Solver = Input::Solver;
+
+  const auto gettime = [&] () {
+    return std::chrono::steady_clock::now();
+  };
+  using TimePoint = decltype(gettime());
+  const auto duration = [&] (const TimePoint& t0, const TimePoint& tf) -> double {
+    return 1e-6*std::chrono::duration_cast<std::chrono::microseconds>(tf - t0).count();
+  };
+
+  const bool on_gpu = scream::util::OnGpu<Kokkos::DefaultExecutionSpace>::value;
+  const int nA = in.oneA ? 1 : in.nrhs;
+
+  TridiagArrays<Real> A("A", in.nprob, 3, in.nrow, nA),
+    Acopy("Acopy", A.extent(0), A.extent(1), A.extent(2), A.extent(3));
+  DataArrays<Real> B("B", in.nprob, in.nrow, in.nrhs),
+    X("X", in.nprob, in.nrow, in.nrhs), Y("Y", in.nprob, in.nrow, in.nrhs);
+  auto Am = create_mirror_view(A);
+  auto Bm = create_mirror_view(B);
+  for (int i = 0; i < in.nprob; ++i) {
+    const auto dl = subview(Am, i, 0, ALL(), ALL());
+    const auto d  = subview(Am, i, 1, ALL(), ALL());
+    const auto du = subview(Am, i, 2, ALL(), ALL());
+    fill_tridiag_matrix(dl, d, du, nA, i);
+    fill_data_matrix(subview(Bm, i, ALL(), ALL()), in.nrhs);
+  }
+  deep_copy(A, Am);
+  deep_copy(B, Bm);
+  deep_copy(Acopy, A);
+  deep_copy(X, B);
+
+  TeamPolicy policy(in.nprob, on_gpu ? 128 : 1, 1);
+  assert(in.nwarp < 0 || ! on_gpu || policy.team_size() == 32*in.nwarp);
+  std::cout << string(in, policy.team_size()/32);
+
+  Kokkos::fence();
+  TimePoint t0, t1;
+  switch (in.method) {
+  case Solver::thomas: {
+    if (on_gpu) {
+      if (in.nrhs == 1) {
+        t0 = gettime();
+#if 0
+        const auto f = KOKKOS_LAMBDA (const typename TeamPolicy::member_type& team) {
+          const int ip = team.league_rank();
+          const auto dl = subview(A, ip, 0, ALL(), 0);
+          const auto d  = subview(A, ip, 1, ALL(), 0);
+          const auto du = subview(A, ip, 2, ALL(), 0);
+          scream::tridiag::thomas(team, dl, d, du, subview(X, ip, ALL(), 0));
+        };
+        parallel_for(policy, f);
+#endif
+        Kokkos::fence();
+        t1 = gettime();
+      }
+    }
+  } break;
+  case Solver::cr: {
+  } break;
+  default:
+    std::cout << "run does not support "
+              << Solver::convert(in.method) << "\n";
+  }
+
+  const auto et = duration(t0, t1);
+  printf("run: et %1.3e et/datum %1.3e\n", et, et/(in.nprob*in.nrow*in.nrhs));
+  Real re; {
+    auto Acopym = create_mirror_view(Acopy);
+    auto Xm = create_mirror_view(X);
+    auto Ym = create_mirror_view(Y);
+    deep_copy(Acopym, Acopy);
+    deep_copy(Xm, X);
+    const auto ip = std::max(0, in.nprob-1);
+    const auto dl = subview(Acopym, ip, 0, ALL(), ALL());
+    const auto d  = subview(Acopym, ip, 1, ALL(), ALL());
+    const auto du = subview(Acopym, ip, 2, ALL(), ALL());
+    matvec(dl, d, du,
+           subview(Xm, in.nprob-1, ALL(), ALL()),
+           subview(Ym, in.nprob-1, ALL(), ALL()),
+           nA, in.nrhs);
+    re = reldif(
+      subview(Bm, in.nprob-1, ALL(), ALL()),
+      subview(Ym, in.nprob-1, ALL(), ALL()),
+      in.nrhs);
+  }
+  if (re > 50*std::numeric_limits<Real>::epsilon())
+    std::cout << "run: " << " re " << re << "\n";
+}
+} // namespace perf
+
 int main (int argc, char **argv) {
   int num_failed = 0;
   scream::initialize_scream_session(argc, argv); {
     if (argc > 1) {
       // Performance test.
-      // TODO
+      perf::Input in;
+      const auto stat = in.parse(argc, argv);
+      if (stat) perf::run<scream::Real>(in);
     } else {
       // Correctness tests.
       num_failed = Catch::Session().run(argc, argv);
