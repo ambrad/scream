747c78e4ca9dedbc257d65b804aedcfae409abe3
Andrew M. Bradley <ambrad@cs.stanford.edu>
2022-07-13 15:32:36 -0500

    work around xml selectors bug

--- components/scream/src/control/atmosphere_driver.cpp
+++ components/scream/src/control/atmosphere_driver.cpp
@@ -111,8 +111,17 @@ set_params(const ekat::ParameterList& atm_params)
 
   m_ad_status |= s_params_set;
 
-  fvphyshack = m_atm_params.sublist("Grids Manager").get<std::string>("Reference Grid") == "Physics PG2";
+  const auto hgn = "Physics PG2";
+  fvphyshack = m_atm_params.sublist("Grids Manager").get<std::string>("Reference Grid") == hgn;
   fprintf(stderr,"amb> fvphyshack %d\n", int(fvphyshack));
+  if (fvphyshack) {
+    // XML selectors don't work for the atmosphere_processes Grids entry; see
+    // issue #1725. Work around this by setting the right values now.
+    auto& pl = const_cast<ekat::ParameterList&>(atm_params.sublist("atmosphere_processes").sublist("physics"));
+    for (const auto c : {"shoc", "cldFraction", "p3"})
+      pl.sublist("mac_aero_mic").sublist(c).set<std::string>("Grid", hgn);
+    pl.sublist("rrtmgp").set<std::string>("Grid", hgn);
+  }
 }
 
 void AtmosphereDriver::
